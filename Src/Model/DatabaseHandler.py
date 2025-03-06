0d3dbb0ad92f1e3cf466455a686a57cdcdbf0c3d
cemilceylan <ceylan.cemil99@gmail.com>
2025-03-06 22:21:44 +0100

    added documentation to db connection functions

--- Src/Model/DatabaseHandler.py
+++ Src/Model/DatabaseHandler.py
@@ -8,14 +8,29 @@ class DatabaseHandler:
         self.__cursor = None
 
     def connect_to_db(self):
+        """
+        Establishes connection to the Database
+
+        :return: None
+        :rtype: None
+        """
         self.__conn = sqlite3.connect("Database.db")
         self.__cursor = self.__conn.cursor()
 
     def close_db_connection(self):
+        """
+        Closes the Database connection
+
+        :return: None
+        :rtype: None
+        """
         try:
             self.__conn.close()
         except AttributeError:
             print("You must open a database connection first")
+
+    def insert_data(self):
+        pass
 
     def get_data(self):
         pass
